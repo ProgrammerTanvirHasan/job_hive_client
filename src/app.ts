b7401de30c18c3c3001641a5661ec375b9cc06ab
Md.Tanvir <hasanmdtanvir916@gmail.com>
2026-04-10 00:45:53 +0600

    setup app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,40 @@
+import { toNodeHandler } from "better-auth/node";
 import express, { Application, Request, Response } from "express";
-
+import { auth } from "./lib/auth";
+import cors from "cors";
+import { applicationRouter } from "./modules/application/application.route";
+import { authRouter } from "./modules/auth/auth.route";
+import { commentRouter } from "./modules/comment/comment.route";
+import { jobRouter } from "./modules/job/job.route";
+import { paymentRouter } from "./modules/payment/payment.route";
+import { userRouter } from "./modules/user/user.route";
+import { voteRouter } from "./modules/vote/vote.route";
 const app: Application = express();
 
 app.use(express.urlencoded({ extended: true }));
 
+const frontendOrigin =
+  process.env.FRONTEND_URL || process.env.CLIENT_URL || "http://localhost:3000";
+
+app.use(
+  cors({
+    origin: frontendOrigin,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    optionsSuccessStatus: 200,
+  }),
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
+
+app.use("/api/application", applicationRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/comment", commentRouter);
+app.use("/api/job", jobRouter);
+app.use("/api/payment", paymentRouter);
+app.use("/api/user", userRouter);
+app.use("/api/vote", voteRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript + Express!!");
